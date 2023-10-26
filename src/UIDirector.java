c36dcff6afb7b3207253c34401c7c41b0379ecb0
Alejandro_p <39816912+minosr3@users.noreply.github.com>
2023-10-25 22:55:27 -0500

    add files

---
+++ src/UIDirector.java
@@ -0,0 +1,15 @@
+
+public class UIDirector {
+
+    private UIBuilder builder;
+
+    public UIDirector(UIBuilder bldr) {
+        builder = bldr;
+    }
+
+    public void build() {
+        builder.addUIControls();
+        builder.initialize();
+    }
+
+}
